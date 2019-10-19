67a4260874c83e3566712c662585c494e87f308a
Enver <39167353+capellac@users.noreply.github.com>
2019-10-19 16:03:55 +0300

    finished composition

--- composition/composition.cpp
+++ composition/composition.cpp
@@ -1,20 +1,36 @@
+#include <string>
 #include <iostream>
+#include "Creature.h"
+#include "Point2D.h"
 
 
 
-class Fraction{
-private:
-	int m_numerator;
-	int m_denominator;
-public:	
-	Fraction(int numerator = 0, int denominator = 1): m_numerator(numerator), m_denominator(denominator){
-		// We put reduce() in the constructor to ensure any fractions we make get reduced!
-		// Since all of the overloaded operators create new Fractions, we can guarantee this will get called here
-		reduce();
-	}
-};
-
-
-int main(){
-
+int main()
+{
+    std::cout << "Enter a name for your creature: ";
+    std::string name;
+    std::cin >> name;
+    Creature creature(name, Point2D(4, 7));
+	
+    while (1)
+    {
+        // print the creature's name and location
+        std::cout << creature << '\n';
+ 
+        std::cout << "Enter new X location for creature (-1 to quit): ";
+        int x=0;
+        std::cin >> x;
+        if (x == -1)
+            break;
+ 
+        std::cout << "Enter new Y location for creature (-1 to quit): ";
+        int y=0;
+        std::cin >> y;
+        if (y == -1)
+            break;
+		
+        creature.moveTo(x, y);
+    }
+ 
+    return 0;
 }

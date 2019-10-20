05fa69d21a7b40008365300933ded631a88e7e0d
Enver <39167353+capellac@users.noreply.github.com>
2019-10-20 21:09:33 +0300

    Update reflexive.cpp

--- association/reflexive/reflexive.cpp
+++ association/reflexive/reflexive.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <string>
 
+class Course{
+private:
+	std::string m_name;
+	Course *m_prerequisite;
+public:
+	Course(std::string name, const Course &prerequisite = nullptr) m_name(name), m_prerequisite(prerequisite){}
+}
+
+
+int main(){
+
+}
